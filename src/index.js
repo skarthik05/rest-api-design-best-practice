de3a5c9ffc82ebd55309f1c883351d9f1fab5b4c
skarthiks05 <karthiks0598@gmail.com>
2022-06-25 17:45:38 +0530

    removed the testing route from the index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1", v1Router);
-// For testing purposes
-app.get("/", (req, res) => {
-  res.send("<h2>It's Working!</h2>");
-});
 
 app.listen(PORT, () => {
   console.log(`API is listening on port : ${PORT}`);
